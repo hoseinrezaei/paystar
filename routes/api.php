cdfe325ed7dc53e8f4a567aa3ab7f57f276ffca4
hoseinrezaei <hosein.rezaei72@gmail.com>
2021-05-23 21:12:56 +0430

    edit relation's in model's  and make route adress

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/posts', [PostController::class, 'index']);
+Route::post('/post', [PostController::class, 'store']);
+Route::get('/post/{id}', [PostController::class, 'show']);
+Route::put('/post/{id}', [PostController::class, 'update']);
+Route::delete('/post/{id}', [PostController::class, 'destroy']);

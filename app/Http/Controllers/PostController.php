c978493f7b9e32734be58e96ca0b284dd30d20c4
hoseinrezaei <hosein.rezaei72@gmail.com>
2021-05-24 00:05:39 +0430

    add all crud function in PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,7 +10,27 @@ class PostController extends Controller
     public function index()
     {
         $posts = Post::select(['id', 'title', 'text']);
-
         dd($posts);
     }
+
+    public function show(Request $request)
+    {
+        //some code
+    }
+
+    public function store(Request $request)
+    {
+        //some code
+    }
+
+    public function update(Request $request)
+    {
+        //some code
+    }
+
+    public function destroy(Request $request)
+    {
+        //some code
+    }
+    
 }
